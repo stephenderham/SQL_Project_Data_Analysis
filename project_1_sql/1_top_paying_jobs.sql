957ec0d1e3702fccd212ed16984bd280749df48f
Stephen Derham <stephen.derham@hotmail.com>
2025-07-24 14:25:01 +1000

    Query_1

---
+++ project_1_sql/1_top_paying_jobs.sql
@@ -0,0 +1,26 @@
+/*
+Focus Question: What are the top-paying data analytst jobs?
+- This will help discover the type of jobs that are the highest paying for a data analyst.
+- Focuses on job postings with specified salaries (with steps in removing null values)
+- Why: To highlight the top opportunities for a data analyst, offering insights into the job market
+*/
+
+SELECT
+    job_id,
+    company_dim.name AS company_name,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date:: DATE
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON
+    company_dim.company_id = job_postings_fact.company_id
+WHERE
+    job_title LIKE '%Data Analyst%' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10;
