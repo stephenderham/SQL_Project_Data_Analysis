f139e3f5fc5edd95d3d5a9f421dc215f26c5dc20
Stephen Derham <stephen.derham@hotmail.com>
2025-07-28 17:25:15 +1000

    upload sql files

--- project_1_sql/4_top_paying_skills.sql
+++ project_1_sql/4_top_paying_skills.sql
@@ -12,9 +12,9 @@ SELECT
     ROUND(AVG(salary_year_avg), 0) AS average_salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON
-    skills_job_dim.job_id = job_postings_fact.job_id
+    job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON 
-    skills_dim.skill_id = skills_job_dim.skill_id
+    skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short LIKE '%Analyst%' AND
     salary_year_avg IS NOT NULL
---
+++ project_1_sql/5_optimal_skills.sql
@@ -0,0 +1,52 @@
+/*
+Focus Question: What are the most optimal skills to learn (high demand and high paying skill)
+- Identifies the skills which are in high demand associated with high average salaries, building upon
+    the previous queries
+- Why? Targets skills that offer job security and financial benefits,
+    offering insights for career development
+*/
+
+WITH skills_demand AS (
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON
+        job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON 
+        skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short LIKE '%Analyst%' AND
+        salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+), average_salary AS (
+    SELECT 
+        skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON
+        job_postings_fact.job_id = skills_job_dim.job_id 
+    INNER JOIN skills_dim ON 
+        skills_job_dim.skill_id = skills_dim.skill_id 
+    WHERE
+        job_title_short LIKE '%Analyst%' AND
+        salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM
+    skills_demand
+INNER JOIN average_salary ON
+    skills_demand.skill_id = average_salary.skill_id
+ORDER BY
+    demand_count DESC, 
+    avg_salary DESC
+LIMIT 25;
